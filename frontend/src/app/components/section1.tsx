178aed4b894ef9a0350e7968de82bfeb412f2d0a
Dashnyam <dashnyam@jayas-MacBook-Pro-2.local>
2025-09-30 17:28:16 +0800

    done

--- frontend/src/app/components/section1.tsx
+++ frontend/src/app/components/section1.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import { MdOutlineContactPage } from "react-icons/md";
 export function Section1() {
   return (
     <div className="mt-8 flex flex-col jutsify-center items-center bg-black">
@@ -8,15 +10,25 @@ export function Section1() {
         Та мөрөөдлийн машинаа
         <div>Xүлээн авах боломжтой</div>
       </h2>
-
-      <Marquee className="gap-100" speed={50} pauseOnHover={true}>
-        {/* <Image width={300} height={100} src={"/1.png"} alt=""></Image> */}
+<Image width={370} height={200}  alt="s"src={"/facebook.png"}></Image>
+ <a
+              href="https://www.facebook.com/dreamcar.mn"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
+            >
+              <div className=" mt-6 flex items-center gap-4 p-4 rounded-xl border-2 border-[#b19155] bg-black hover:bg-[#b19155] transition-colors duration-300 cursor-pointer shadow-lg">
+                <MdOutlineContactPage className="w-6 h-6 text-[#b19155] hover:text-black transition-colors duration-300" />
+                <h1 className="text-md font-semibold text-[#b19155] hover:text-black transition-colors duration-300">
+                  Page Хуудас-руу очих
+                </h1>
+              </div>
+            </a>
+      {/* <Marquee className="gap-100" speed={50} pauseOnHover={true}>
         <Image width={300} height={100} src={"/4.png"} alt=""></Image>
-        {/* <Image width={300} height={100} src={"/3.png"} alt=""></Image> */}
-        {/* <Image width={300} height={100} src={"/2.png"} alt=""></Image> */}
         <Image width={300} height={100} src={"/5.png"} alt=""></Image>
         <Image width={300} height={100} src={"/6.png"} alt=""></Image>
-      </Marquee>
+      </Marquee> */}
       {/* <div className="bg-white rounded-lg p-4 w-full">
         <img src="/sample.png" className="rounded-lg w-full h-60 object-cover"></img>
       </div> */}
